240a2173310cfe34d72b0f410296dc212a3bdb5a
Claudio Suardi <claudio@nearit.com>
2019-10-17 12:25:16 +0200

    chore(various): improve sample, remove unused test, bumps version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxFreshChatService } from 'projects/ngx-freshchat-lib/src/public_api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  title = 'ngx-freshchat Sample';
+
+  constructor(private chat: NgxFreshChatService) {}
+
+  ngOnInit(): void {
+    /* this.chat.init({
+      token: 'YOUR_FC_TOKEN',
+      host: 'YOUR_FC_URL'
+    })
+    .subscribe(
+        () => console.log('FreshChat is ready!')
+    ); */
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxFreshChatModule } from 'ngx-freshchat-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxFreshChatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
